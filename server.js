269525c7904af2d90d309e8f7122925773ec616f
ignurof <ignurof@outlook.com>
2021-09-21 10:58:15 +0200

    working edition with basic routing and one file structure

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 3111;
 const server = http.createServer((request, response) => {
 	// Header options that gets returned to client browser during preflight request
 	const headers = {
-		"Access-Control-Allow-Origin": "*",
+		"Access-Control-Allow-Origin": "api.ignurof.xyz",
 		"Access-Control-Allow-Methods": "OPTIONS, POST, GET",
 		"Access-Control-Max-Age": 2592000, // 30 days stored on browser cache TODO: Figure out optimal max-age
 		/** add other headers as per requirement */
@@ -20,11 +20,13 @@ const server = http.createServer((request, response) => {
 		response.end();
 		return;
 	  }
-
+	  // den var utan !! först och kollade istället > -1,  då funkade den. TODO: ta reda på varför detta inte funkar
+	  // Not Not False = True, means if GET or POST is available, this is true
+	  // if(var) = if true, (!var) = if not true, (!!var) == if not not true = true
 	  if (["GET", "POST"].indexOf(request.method) > -1) {
 			response.writeHead(200, headers);
 			// OK/SUCCESS response to client here
-			var url = request.url;
+			let url = request.url;
 
 		// API Routing https://www.example.com?name=n1&name=n2
 		if(url == "/about"){
@@ -43,11 +45,12 @@ const server = http.createServer((request, response) => {
 			return CreateJSON(response);
 		}
 
-		// if main root path
+			// if no route is chosen, give this result
 			return DefaultPage(response);;
 	  }
 
 	  // The early returns should make sure the client doesnt end up here
+	  // We end up here if["GET", "POST"].indexOf(request.method)=false
 	  response.writeHead(405, headers);
 	  response.end(`${request.method} is not allowed for the request.`);
 });
