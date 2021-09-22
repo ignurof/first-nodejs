5a19ee79ad4a12037745402f2ce177c369d3191a
ignurof <ignurof@outlook.com>
2021-09-22 16:11:33 +0200

    test some stuff with createjson, this file is only for testing purposes and learning js

--- createjson.js
+++ createjson.js
@@ -4,15 +4,28 @@ const fs = require("fs");
 const ErrorPage = require("./errors.js");
 
 const CreateJSON = (response) => {
-	let testProjects = {
-		"id": 1,
-		"title": "hej",
-		"summary": "sad",
-		"content": "asdasd",
-		"images": ["one", "two", "three", "four"]
+	let aProject = {
+		"id": "test",
+		"title": "test",
+		"summary": "test",
+		"content": "t",
+		"images": ["t", "t", "t", "t"]
 	};
 
-	let inputObject = JSON.stringify(testProjects);
+	let manyProjects = {
+		"projects": [
+			aProject,
+			{
+				"id": "test2",
+				"title": "test2",
+				"summary": "test2",
+				"content": "t",
+				"images": ["t", "t", "t", "t"]
+			}
+		]
+	};
+
+	let inputObject = JSON.stringify(manyProjects);
 
 	// Create JSON-file
 	fs.writeFile("projects.json", inputObject, (error) => {
