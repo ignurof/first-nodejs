be84e0ec228bd273cea3750b9862880615dd54ba
ignurof <ignurof@outlook.com>
2021-09-24 01:11:08 +0200

    try const instead i dont even know

--- projects.js
+++ projects.js
@@ -20,15 +20,17 @@ const AddProject = (id, title, summary, content, images) => {
 
 const GenerateProjectList = (response) => {
 	// Add new project to projectList
-	AddProject("1", "Jetpack Doggo 1", "C#, Unity", "Text about the game", ["jp1.jpg", "jp2.jpg", "jp3.jpg", "jp4.jpg"]);
-	console.log(projectList);
-	// Convert object to string so we can use it in fs
-	let inputObject = JSON.parse(projectList);
+	AddProject(1, "Jetpack Doggo 1", "C#, Unity", "Text about the game", ["jp1.jpg", "jp2.jpg", "jp3.jpg", "jp4.jpg"]);
+	
+	// convert JSON object to string
+	const data = JSON.stringify(projectList);
+
+	console.log(data);
 
 	// TODO FOR THE MORNING: Make it so it wont do this if the file already exists, and make it in projects.js so that if that doesnt exist then use this method
 	// Make all page method more modular by putting response in the routing area instead, maybe just end so I can pass data through the response with the methods
 	// Create new file
-	fs.writeFile("projectList.json", inputObject, (error) => {
+	fs.writeFile("projectList.json", data, (error) => {
 		if(error) return ErrorPage(response);
 	});
 }
