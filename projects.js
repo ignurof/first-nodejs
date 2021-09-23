afc19444fda433aed7a5e0b178e463dc81b2339d
ignurof <ignurof@outlook.com>
2021-09-24 01:19:35 +0200

    more error handling

--- projects.js
+++ projects.js
@@ -18,7 +18,7 @@ const AddProject = (id, title, summary, content, images) => {
 	projectList["projects"].push(project);
 }
 
-const GenerateProjectList = (response) => {
+const GenerateProjectList = () => {
 	// Add new project to projectList
 	AddProject(1, "Jetpack Doggo 1", "C#, Unity", "Text about the game", ["jp1.jpg", "jp2.jpg", "jp3.jpg", "jp4.jpg"]);
 	
@@ -31,7 +31,7 @@ const GenerateProjectList = (response) => {
 	// Make all page method more modular by putting response in the routing area instead, maybe just end so I can pass data through the response with the methods
 	// Create new file
 	fs.writeFile("projectList.json", data, (error) => {
-		if(error) return ErrorPage(response);
+		if(error) throw error;
 	});
 }
 
@@ -39,9 +39,16 @@ const ProjectsPage = (response) => {
 	// Open filestream and try to read the file
 	fs.readFile("projectList.json", (error, data) => {
 		// 404 - If file does not exist, or other error
+		let isError = false;
 		if(error){
-			throw error;
+			console.log(error);
+			isError = true;
 		}
+		// If the file does not exist
+		if(isError){
+			GenerateProjectList(response);
+		}
+
 		// Write the data to the client
 		response.write(data);
 		// End the response so client recieve it
