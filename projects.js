41eecd289f4d167743dadc45c719a18ed2255fd4
ignurof <ignurof@outlook.com>
2021-09-24 13:46:29 +0200

    Just make projects work, dont know what I was doing.

--- projects.js
+++ projects.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
-const ErrorPage = require("./errors.js");
 
 let projectList = {
 	"projects": []
 };
 
+// Adds a new project to the list. TODO: Make it so it appends to the actual file
 const AddProject = (id, title, summary, content, images) => {
 	let project = {
 		"id": id,
@@ -18,49 +18,61 @@ const AddProject = (id, title, summary, content, images) => {
 	projectList["projects"].push(project);
 }
 
-const GenerateProjectList = () => {
+// Respond with the projectList
+const GetProjectList = (response) => {
+	// Open filestream and try to read the file
+	fs.readFile("projectList.json", (error, data) => {
+		// 404 - If file does not exist, or other error
+		if(error){
+			console.log("read error"); // DEBUG
+		}
+
+		// Send to client
+		response.write(data);
+		response.end();
+	});
+}
+
+// If the projectList didnt exist, create it and respond with it
+const GenerateProjectList = (response) => {
 	// Add new project to projectList
 	AddProject(1, "Jetpack Doggo 1", "C#, Unity", "Text about the game", ["jp1.jpg", "jp2.jpg", "jp3.jpg", "jp4.jpg"]);
 	
 	// convert JSON object to string
-	const data = JSON.stringify(projectList);
+	let data = JSON.stringify(projectList);
 
-	console.log(data);
+	console.log(data); // DEBUG
 
-	// TODO FOR THE MORNING: Make it so it wont do this if the file already exists, and make it in projects.js so that if that doesnt exist then use this method
-	// Make all page method more modular by putting response in the routing area instead, maybe just end so I can pass data through the response with the methods
 	// Create new file
 	fs.writeFile("projectList.json", data, (error) => {
-		if(error) throw error;
-	});
-}
-
-const ReadProjectsList = () => {
-	// Open filestream and try to read the file
-	fs.readFile("projectList.json", (error, data) => {
-		// 404 - If file does not exist, or other error
-		if(error) throw error;
-
-		return data;
-	});
-}
-
-const ProjectsPage = (response) => {
-	//Check if file exists first
-	fs.access("projectList.json", (error) => {
-		// If file does not exist, aka throws error
 		if(error){
-			console.log("access error");
-			GenerateProjectList();
+			console.log("write error"); // DEBUG
+		}
+
+		console.log("projectList.json created");
+	});
+
+	// Send to client
+	response.write(data);
+	response.end();
+}
+
+// Respond with appropriate projects method, but the gist of it is respond with projectList.json
+const ProjectsPage = (response) => {
+	// Check if projectList.json exist
+	fs.stat("projectList.json", (error, stat) => {
+		if(error == null) {
+			// If the file exists
+			console.log('File exists');
+			return GetProjectList(response);
+		} else if(error.code === 'ENOENT') {
+			// If the file does not exist
+			console.log("File does not exist");
+			return GenerateProjectList(response);
+		} else {
+			throw error;
 		}
 	});
-
-	// If file exists, call the method that returns that contents of file
-	let output = ReadProjectsList();
-	// Write the data to client
-	response.write(output);
-	// Send to client
-	response.end();
 }
 
 
