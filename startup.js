1e8c9caa5a03899d3bfd921da9cc87d3c35a3bd6
ignurof <ignurof@outlook.com>
2021-10-03 15:36:22 +0200

    fix new addproject values

--- startup.js
+++ startup.js
@@ -14,7 +14,7 @@ const ValidateProjects = () => {
 			// Adds a new project to projectList object and then runs CreateProjectListJSON() to create or overwrite the file
 			projects.AddProject("Jetpack Doggo 1", "C#, Unity", "Text about the game", ["jp1.jpg", "jp2.jpg", "jp3.jpg", "jp4.jpg"]);
 			// Since I decided to remake the entire website with a new tech stack and then make a proper admin frontend, im manually making all projects here
-			projects.AddProject("Jetpack Doggo 2", "C#, Unity", "Text about the game", ["jp2-1.jpg", "jp2-2.jpg", "jp2-3.jpg", "jp2-4.jpg"]);
+			projects.AddProject("Jetpack Doggo 2", "C#, Unity", "Text about the game", ["jp21.jpg", "jp22.jpg", "jp23.jpg", "jp24.jpg"]);
 		} else {
 			throw error;
 		}
